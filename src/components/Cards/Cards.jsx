afa042d55174dda6970892d5f0f0586e29e66a96
RichardPitts1 <Richardepitts@gmail.com>
2020-08-23 13:35:01 -0500

    2

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+import styles from './Cards.module.css'
+
+const Cards = ({confirmed, recovered, deaths, lastUpdate}) => {
+    if(!confirmed) {
+        return 'Loading...'
+    }
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography variant="h5">{confirmed.value}</Typography>
+                    <Typography color="textSecondary">Real Date</Typography>
+                    <Typography variant="body2">Number of active cases of Covid 19</Typography>
+                </Grid>
+                <Grid item component={Card}>
+                <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                <Typography variant="h5">Real Data</Typography>
+                <Typography color="textSecondary">Real Date</Typography>
+                <Typography variant="body2">Number of recoveries from Covid 19</Typography>
+            </Grid>
+            <Grid item component={Card}>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">Number of deaths from Covid 19</Typography>
+        </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Cards;
