245a85db8541c52c910f3bec7343e4e260ac63e0
RichardPitts1 <Richardepitts@gmail.com>
2020-08-24 12:57:51 -0500

    2

--- src/api/index.js
+++ src/api/index.js
@@ -17,10 +17,21 @@ export const fetchData = async () => {
         }
 }
 
-export const fetchDailyData = async() => {
+export const fetchDailyData = async () => {
     try {
-        const { data } = await axios.get(`${url}/daily`)
-        console.log(data);
-    } catch (error){
+      const { data } = await axios.get(`${url}/daily`);
+  
+      return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+      return error;
+    }
+};
+
+export const fetchCountries = async () => {
+    try  {
+        const {data: { countries }} = await axios.get(`${url}/countries`)
+        return countries.map((country) => country.name);
+    } catch (error) {
+        console.log(error)
     }
 }
